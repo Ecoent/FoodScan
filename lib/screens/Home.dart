0dac5445807541ec27f0f790cb21354813129fbd
qsvtr <47601451+qsvtr@users.noreply.github.com>
2019-04-24 15:41:11 +0200

    Initial commit

---
+++ lib/screens/Home.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:food_scan/styles/customText.dart';
+import 'package:food_scan/styles/customBoxDecoration.dart';
+import 'package:food_scan/screens/ArticleDetail.dart';
+import 'package:food_scan/widgets/drawerHome.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  String result = "";
+  bool debug = false;
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+        Navigator.push(context, new MaterialPageRoute(
+            builder: (BuildContext context) {
+              return new Article(url : 'https://world.openfoodfacts.org/api/v0/product/'+result+'.json');
+            })
+        );
+      });
+    }
+    on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) { setState(() { result = "Camera permission was denied"; }); }
+      else { setState(() { result = "Unknown Error $ex"; }); }
+    }
+    on FormatException { setState(() { result = "You pressed the back button before scanning anything"; }); }
+    catch (ex) { setState(() { result = "Unknown Error $ex"; }); }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Food Scan"),
+      ),
+      drawer: new DrawerHome(),
+      body: new Center(
+        child: new ListView(
+          //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ListView(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(20.0),
+                children: <Widget>[
+                  CustomText("Bienvenue !", factor: 2.0,),
+                  new Container(
+                    alignment: Alignment.center,
+                    decoration: debug ? new CustomBoxDecoration() : null,
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Merci à",),
+                        FlatButton(
+                          padding: EdgeInsets.all(0.0),
+                          child: Text("OpenFoodFacts", style: TextStyle(decoration: TextDecoration.underline),),
+                          onPressed: _launchOpenFoodFacts,
+                        ),
+                        Text(" pour leur base de données libre  "),
+                        Image.asset('lib/images/openFoodFactsLogo.png', scale: 5.0,),
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ]
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  _launchOpenFoodFacts() async {
+    const url = 'https://world.openfoodfacts.org';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
---
+++ lib/widgets/drawerHome.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:food_scan/styles/customText.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DrawerHome extends Drawer {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Drawer(
+      child: new Column(
+        children: <Widget>[
+          new UserAccountsDrawerHeader(
+            accountName: new Text("Quentin SAUVETRE"),
+            accountEmail: new Text("apps@quentin-sauvetre.fr"),
+            currentAccountPicture: CircleAvatar(
+              child: FlutterLogo(),
+              //backgroundColor: Colors.blue,
+            ),
+            otherAccountsPictures: <Widget>[
+              new CustomText(
+                "v0.1",
+                factor: 1.1,
+              ),
+            ],
+          ),
+          new Column(
+            children: <Widget>[
+              ListTile(
+                leading: new Icon(Icons.help),
+                title: Text("A propos de l'application"),
+                onTap: () {
+                  //
+                },
+              ),
+              Divider(
+                height: 1.0,
+              ),
+              ListTile(
+                leading: new Icon(Icons.mail),
+                title: Text('Contactez moi'),
+                onTap: () {
+                  _launchMail();
+                },
+              ),
+              Divider(
+                height: 1.0,
+              ),
+              ListTile(
+                leading: new Icon(Icons.input),
+                title: Text('Visitez mon site web'),
+                onTap: () {
+                  _launchWebsite();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _launchMail() async {
+    await launch(
+        'mailto:apps@quentin-sauvetre.fr?subject=Information&body=Hi Quentin,\n\n');
+  }
+
+  _launchWebsite() async {
+    const url = 'https://quentin-sauvetre.fr';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
